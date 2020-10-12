7df8ed4d3a1eb2b8d10b120d0d12bba725847003
Crystal Norman <cnorm88@gmail.com>
2020-10-12 01:19:19 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,24 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  # def reset_all
+  #   self.all.clear
+  # end
 
-  def count
-    self.all.count
+  # def count
+  #   self.all.count
+  # end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
   end
 end
